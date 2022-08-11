07cd1ac69a71efc6f41ae2c3fea2fd4d1377bfaf
Kgaogelo Thorn <kgaogelo@swipeix.com>
2022-08-11 14:34:32 +0200

    html tips

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var person = new Object();
 person["firstName"] = "Bill";
 person["lastName"] = "Russel";
+
 var firstNameProperty = "firstName";
+
 console.log(person);
 console.log(person[firstNameProperty]);
+console.log(person.lastName);
