79d852434373b5a21157ff49361e93e07e45f561
Kgaogelo Thorn <kgaogelo@swipeix.com>
2022-08-18 10:09:11 +0200

    es6

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 var a = $('ul.content').addClass('new-class');
 console.log(a);
+
+//* Promises, Async and Await
+fetch('video.json')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
